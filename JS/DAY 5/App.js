307c2674526999f903fb8cb0231a58942fd330b6
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-08-22 19:55:00 +0530

    Add files via upload

---
+++ JS/DAY 5/App.js
@@ -0,0 +1,29 @@
+console.log(10 + 10);
+
+//! IMPLICIT TYPECASTING:
+console.log(10 + "str"); //! number is converted into string
+console.log("str" + 60);
+console.log(100 - "10"); //!string converrted into number.
+console.log(100 - "str"); //! NaN
+console.log(100 * "5"); //! string is converted into number.
+console.log(100 / 10);
+
+//! JS OUTPUT METHODS
+console.log("javascript");
+
+document.writeln("REACT JS");
+document.write("WEB TECHNOLOGY");
+document.write("EXPRESS JS")
+
+window.alert("alert popup");
+
+// let res = window.confirm("are you sure? Do u want to continue");
+// console.log(res);
+// if (res) console.log("page is loading");
+// else console.log("error");
+
+// let age = prompt("Enter Your Age");
+let ageRes = parseInt(age);
+// console.log(typeof age);
+console.log(ageRes);
+console.log(typeof ageRes);
